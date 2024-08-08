e5d0d70b4d3e4a99e0b2b9c6064de264033d03f5
Het Chauhan <121594899+ChauhanHet212@users.noreply.github.com>
2024-08-08 23:30:00 +0530

    Update Helper.kt

--- Helper.kt
+++ Helper.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.withContext
 //-------------Coroutine----------------
 /**
  * This is the Main thread coroutine scope to use anywhere
- * @param block - code block to run on this thread
+ * @param block -> code block to run on this thread
  */
 fun mainThread(block: CoroutineScope.() -> Unit) {
     CoroutineScope(Dispatchers.Main).launch { block() }
@@ -18,7 +18,7 @@ fun mainThread(block: CoroutineScope.() -> Unit) {
 
 /**
  * This is the IO thread coroutine scope to use anywhere
- * @param block - code block to run on this thread
+ * @param block -> code block to run on this thread
  */
 fun ioThread(block: CoroutineScope.() -> Unit) {
     CoroutineScope(Dispatchers.IO).launch { block() }
@@ -26,7 +26,7 @@ fun ioThread(block: CoroutineScope.() -> Unit) {
 
 /**
  * This is the Default thread coroutine scope to use anywhere
- * @param block - code block to run on this thread
+ * @param block -> code block to run on this thread
  */
 fun defaultThread(block: CoroutineScope.() -> Unit) {
     CoroutineScope(Dispatchers.Default).launch { block() }
@@ -34,7 +34,7 @@ fun defaultThread(block: CoroutineScope.() -> Unit) {
 
 /**
  * This is the Unconfined thread coroutine scope to use anywhere
- * @param block - code block to run on this thread
+ * @param block -> code block to run on this thread
  */
 fun unconfinedThread(block: CoroutineScope.() -> Unit) {
     CoroutineScope(Dispatchers.Unconfined).launch { block() }
@@ -43,7 +43,7 @@ fun unconfinedThread(block: CoroutineScope.() -> Unit) {
 
 /**
  * This is the Main thread coroutine scope to use inside other coroutine scope
- * @param block - code block to run on this thread
+ * @param block -> code block to run on this thread
  */
 suspend fun innerMainThread(block: suspend CoroutineScope.() -> Unit) {
     withContext(Dispatchers.Main) { block() }
@@ -51,7 +51,7 @@ suspend fun innerMainThread(block: suspend CoroutineScope.() -> Unit) {
 
 /**
  * This is the IO thread coroutine scope to use inside other coroutine scope
- * @param block - code block to run on this thread
+ * @param block -> code block to run on this thread
  */
 suspend fun innerIOThread(block: suspend CoroutineScope.() -> Unit) {
     withContext(Dispatchers.IO) { block() }
@@ -59,7 +59,7 @@ suspend fun innerIOThread(block: suspend CoroutineScope.() -> Unit) {
 
 /**
  * This is the Default thread coroutine scope to use inside other coroutine scope
- * @param block - code block to run on this thread
+ * @param block -> code block to run on this thread
  */
 suspend fun innerDefaultThread(block: suspend CoroutineScope.() -> Unit) {
     withContext(Dispatchers.Default) { block() }
@@ -67,7 +67,7 @@ suspend fun innerDefaultThread(block: suspend CoroutineScope.() -> Unit) {
 
 /**
  * This is the Unconfined thread coroutine scope to use inside other coroutine scope
- * @param block - code block to run on this thread
+ * @param block -> code block to run on this thread
  */
 suspend fun innerUnconfinedThread(block: suspend CoroutineScope.() -> Unit) {
     withContext(Dispatchers.Unconfined) { block() }
@@ -78,9 +78,9 @@ suspend fun innerUnconfinedThread(block: suspend CoroutineScope.() -> Unit) {
 //-------------Api levels(android versions)----------------
 /**
  * To run block of code on above given api level
- * @param apiLevel - the api level to check
- * @param block - block of code to run if api level is above given api level
- * @param elseBlock - block of code to run if api level is not above given api level
+ * @param apiLevel -> the api level to check
+ * @param block -> block of code to run if api level is above given api level
+ * @param elseBlock -> block of code to run if api level is not above given api level
  */
 fun aboveApi(apiLevel: Int, elseBlock: () -> Unit = {}, block: () -> Unit) {
     if (Build.VERSION.SDK_INT > apiLevel) {
@@ -92,9 +92,9 @@ fun aboveApi(apiLevel: Int, elseBlock: () -> Unit = {}, block: () -> Unit) {
 
 /**
  * To run block of code on above or given api level
- * @param apiLevel - the api level to check
- * @param block - block of code to run if api level is above or same as given api level
- * @param elseBlock - block of code to run if api level is not above or not same as given api level
+ * @param apiLevel -> the api level to check
+ * @param block -> block of code to run if api level is above or same as given api level
+ * @param elseBlock -> block of code to run if api level is not above or not same as given api level
  */
 fun onOrAboveApi(apiLevel: Int, elseBlock: () -> Unit = {}, block: () -> Unit) {
     if (Build.VERSION.SDK_INT >= apiLevel) {
@@ -106,9 +106,9 @@ fun onOrAboveApi(apiLevel: Int, elseBlock: () -> Unit = {}, block: () -> Unit) {
 
 /**
  * To run block of code on below given api level
- * @param apiLevel - the api level to check
- * @param block - block of code to run if api level is below given api level
- * @param elseBlock - block of code to run if api level is not below given api level
+ * @param apiLevel -> the api level to check
+ * @param block -> block of code to run if api level is below given api level
+ * @param elseBlock -> block of code to run if api level is not below given api level
  */
 fun belowApi(apiLevel: Int, elseBlock: () -> Unit = {}, block: () -> Unit) {
     if (Build.VERSION.SDK_INT < apiLevel) {
@@ -120,9 +120,9 @@ fun belowApi(apiLevel: Int, elseBlock: () -> Unit = {}, block: () -> Unit) {
 
 /**
  * To run block of code on below or given api level
- * @param apiLevel - the api level to check
- * @param block - block of code to run if api level is below or same as given api level
- * @param elseBlock - block of code to run if api level is not below or not same as given api level
+ * @param apiLevel -> the api level to check
+ * @param block -> block of code to run if api level is below or same as given api level
+ * @param elseBlock -> block of code to run if api level is not below or not same as given api level
  */
 fun onOrBelowApi(apiLevel: Int, elseBlock: () -> Unit = {}, block: () -> Unit) {
     if (Build.VERSION.SDK_INT <= apiLevel) {
@@ -134,9 +134,9 @@ fun onOrBelowApi(apiLevel: Int, elseBlock: () -> Unit = {}, block: () -> Unit) {
 
 /**
  * To run block of code on given api level
- * @param apiLevel - the api level to check
- * @param block - block of code to run if api level is same as given api level
- * @param elseBlock - block of code to run if api level is not same as given api level
+ * @param apiLevel -> the api level to check
+ * @param block -> block of code to run if api level is same as given api level
+ * @param elseBlock -> block of code to run if api level is not same as given api level
  */
 fun onApi(apiLevel: Int, elseBlock: () -> Unit = {}, block: () -> Unit) {
     if (Build.VERSION.SDK_INT == apiLevel) {
@@ -148,9 +148,9 @@ fun onApi(apiLevel: Int, elseBlock: () -> Unit = {}, block: () -> Unit) {
 
 /**
  * To run block of code on given api levels
- * @param apiLevels - the api levels to check
- * @param block - block of code to run if api level is in given api levels
- * @param elseBlock - block of code to run if api level is not in given api levels
+ * @param apiLevels -> the api levels to check
+ * @param block -> block of code to run if api level is in given api levels
+ * @param elseBlock -> block of code to run if api level is not in given api levels
  */
 fun onApis(vararg apiLevels: Int, elseBlock: () -> Unit = {}, block: () -> Unit) {
     if (apiLevels.contains(Build.VERSION.SDK_INT)) {
@@ -162,8 +162,8 @@ fun onApis(vararg apiLevels: Int, elseBlock: () -> Unit = {}, block: () -> Unit)
 
 /**
  * To run block of code of a particular api levels
- * @param levelsAndBlocks - pair of api level and block of code to run on that api level
- * @param elseBlock - if api level is not in given api levels
+ * @param levelsAndBlocks -> pair of api level and block of code to run on that api level
+ * @param elseBlock -> if api level is not in given api levels
  */
 fun whenApiLevel(vararg levelsAndBlocks: Pair<Int, () -> Unit>, elseBlock: () -> Unit = {}) {
     if (levelsAndBlocks.map { it.first }.contains(Build.VERSION.SDK_INT)) {
