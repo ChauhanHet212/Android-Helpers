6e802ef922cdc618f700f437d11b1908712fa196
Het Chauhan <121594899+ChauhanHet212@users.noreply.github.com>
2024-08-18 16:38:10 +0530

    Update HelperExt.kt

--- HelperExt.kt
+++ HelperExt.kt
@@ -1,23 +1,41 @@
 package com.example.helper.extras
 
 import android.animation.TimeInterpolator
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.AppOpsManager
 import android.content.Context
 import android.content.Intent
 import android.content.Intent.getIntent
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Point
+import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.os.Build
+import android.text.format.DateUtils
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.ViewGroup.MarginLayoutParams
+import android.view.WindowManager
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.inputmethod.InputMethodManager
 import android.widget.GridLayout
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TableLayout
+import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.annotation.DimenRes
 import androidx.annotation.DrawableRes
+import androidx.annotation.RequiresApi
+import androidx.annotation.UiThread
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.marginBottom
@@ -26,16 +44,24 @@ import androidx.core.view.marginRight
 import androidx.core.view.marginTop
 import androidx.core.view.setMargins
 import androidx.core.view.updateMargins
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
+import androidx.viewbinding.ViewBinding
+import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.Serializable
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Collections
+import java.util.Date
 
 
 //-------------Coroutine----------------
@@ -102,6 +128,14 @@ fun ViewModel.defaultThread(block: CoroutineScope.() -> Unit) {
  */
 fun ViewModel.unconfinedThread(block: CoroutineScope.() -> Unit) {
     viewModelScope.launch(Dispatchers.Unconfined) { block() }
+}
+
+/**
+ * This is the ui thread for Activity
+ * @param block -> code block to run on this thread
+ */
+fun Activity.uiThread(block: () -> Unit) {
+    runOnUiThread { block() }
 }
 
 
@@ -349,3 +383,560 @@ fun Context.getResourceDrawable(@DrawableRes drawable: Int): Drawable? {
 fun Context.getResourceDimen(@DimenRes dimen: Int): Float {
     return ResourcesCompat.getFloat(this.resources, dimen)
 }
+
+
+
+//-------------Activity----------------
+/**
+ * Hide keyboard for an activity.
+ */
+fun Activity.hideSoftKeyboard() {
+    if (currentFocus != null) {
+        val inputMethodManager = getSystemService(
+            Context
+                .INPUT_METHOD_SERVICE
+        ) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+    }
+}
+
+/**
+ * Show keyboard for an activity.
+ * @param toFocus -> View to show keyboard on
+ */
+fun Activity.showKeyboard(toFocus: View) {
+    toFocus.requestFocus()
+    val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(toFocus, InputMethodManager.SHOW_IMPLICIT);
+//    imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, InputMethodManager.HIDE_IMPLICIT_ONLY)
+}
+
+/**
+ *  get display size in pixels
+ */
+fun Activity.getDisplaySizePixels(): Point {
+    val display = this.windowManager.defaultDisplay
+    return DisplayMetrics()
+        .apply {
+            display.getRealMetrics(this)
+        }.let {
+            Point(it.widthPixels, it.heightPixels)
+        }
+}
+
+/**
+ * Set Status Bar Color
+ */
+fun Activity.setStatusBarColor(@ColorInt color: Int) {
+    window.statusBarColor = color
+}
+
+/**
+ * Set Navigation Bar Color
+ */
+fun Activity.setNavigationBarColor(@ColorInt color: Int) {
+    window.navigationBarColor = color
+}
+
+/**
+ * Set Navigation Bar Divider Color if Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
+ */
+@RequiresApi(api = Build.VERSION_CODES.P)
+fun Activity.setNavigationBarDividerColor(@ColorInt color: Int) {
+    window.navigationBarDividerColor = color
+}
+
+/**
+ *  Makes the activity enter fullscreen mode.
+ */
+@UiThread
+fun Activity.enterFullScreenMode() {
+    window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+}
+
+
+/**
+ * Makes the Activity exit fullscreen mode.
+ */
+@UiThread
+fun Activity.exitFullScreenMode() {
+    window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+}
+
+/**
+ * Hide navigation buttons
+ */
+@SuppressLint("ObsoleteSdkInt")
+fun Activity.hideBottomBar() {
+    if (Build.VERSION.SDK_INT < 19) { // lower api
+        val v = this.window.decorView
+        v.systemUiVisibility = View.GONE
+    } else {
+        //for new api versions.
+        val decorView = window.decorView
+        val uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        decorView.systemUiVisibility = uiOptions
+    }
+}
+
+/**
+ * Show navigation buttons
+ */
+@SuppressLint("ObsoleteSdkInt")
+fun Activity.showBottomBar() {
+    if (Build.VERSION.SDK_INT < 19) { // lower api
+        val v = this.window.decorView
+        v.systemUiVisibility = View.VISIBLE
+    } else {
+        //for new api versions.
+        val decorView = window.decorView
+        val uiOptions = View.SYSTEM_UI_FLAG_VISIBLE
+        decorView.systemUiVisibility = uiOptions
+    }
+}
+
+/**
+ * Lock screen orientation
+ */
+fun Activity.lockOrientation() {
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_NOSENSOR
+}
+
+/**
+ * Lock screen orientation Portrait
+ */
+@SuppressLint("SourceLockedOrientationActivity")
+fun Activity.lockOrientationPortrait() {
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+}
+
+/**
+ * Lock screen orientation Horizontal
+ */
+fun Activity.lockOrientationHorizontal() {
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+}
+
+/**
+ * Unlock screen orientation
+ */
+fun Activity.unlockScreenOrientation() {
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR
+}
+
+/**
+ * Get screenShort as bitmap
+ */
+fun Activity.screenShot(removeStatusBar: Boolean = false): Bitmap? {
+    val dm = DisplayMetrics()
+    windowManager.defaultDisplay.getMetrics(dm)
+
+    val bmp = Bitmap.createBitmap(dm.widthPixels, dm.heightPixels, Bitmap.Config.RGB_565)
+    val canvas = Canvas(bmp)
+    window.decorView.draw(canvas)
+
+
+    return if (removeStatusBar) {
+        val rect = Rect()
+        window.decorView.getWindowVisibleDisplayFrame(rect)
+        val statusBarHeight = rect.top
+        Bitmap.createBitmap(
+            bmp,
+            0,
+            statusBarHeight,
+            dm.widthPixels,
+            dm.heightPixels - statusBarHeight
+        )
+    } else {
+        Bitmap.createBitmap(bmp, 0, 0, dm.widthPixels, dm.heightPixels)
+    }
+}
+
+/**
+ * Check that the device has permission for picture in picture mode or not
+ */
+fun Activity.hasPipPermission(): Boolean {
+    val appOps = getSystemService(AppOpsManager::class.java)
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            appOps?.unsafeCheckOpNoThrow(
+                AppOpsManager.OPSTR_PICTURE_IN_PICTURE,
+                android.os.Process.myUid(),
+                packageName
+            ) == AppOpsManager.MODE_ALLOWED
+        } else {
+            appOps?.checkOpNoThrow(
+                AppOpsManager.OPSTR_PICTURE_IN_PICTURE,
+                android.os.Process.myUid(),
+                packageName
+            ) == AppOpsManager.MODE_ALLOWED
+        }
+    } else {
+        return try {
+            packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
+
+
+//-------------Binding----------------
+/**
+ * To get viewBinding in Activity
+ */
+inline fun <reified T : ViewBinding> Activity.viewBinding(
+    parent: ViewGroup? = null,
+    attachToParent: Boolean = false,
+): Lazy<T> {
+    return object : Lazy<T> {
+
+        private var cached: T? = null
+
+        override val value: T
+            get() {
+                if (cached == null) {
+                    cached = T::class.java.getDeclaredMethod(
+                        "inflate", LayoutInflater::class.java,
+                        ViewGroup::class.java, Boolean::class.java
+                    ).invoke(null, layoutInflater, parent, attachToParent) as T
+                }
+                return cached!!
+            }
+
+        override fun isInitialized() = cached != null
+    }
+}
+
+/**
+ * To get viewBinding in Fragment
+ */
+inline fun <reified T : ViewBinding> Fragment.viewBinding(
+    parent: ViewGroup? = null,
+    attachToParent: Boolean = false,
+): Lazy<T> {
+    return object : Lazy<T> {
+
+        private var cached: T? = null
+
+        override val value: T
+            get() {
+                if (cached == null) {
+                    cached = T::class.java.getDeclaredMethod(
+                        "inflate", LayoutInflater::class.java,
+                        ViewGroup::class.java, Boolean::class.java
+                    ).invoke(null, layoutInflater, parent, attachToParent) as T
+                }
+                return cached!!
+            }
+
+        override fun isInitialized() = cached != null
+    }
+}
+
+/**
+ * To get viewBinding using Context
+ */
+inline fun <reified T : ViewBinding> Context.viewBinding(
+    parent: ViewGroup? = null,
+    attachToParent: Boolean = false,
+): Lazy<T> {
+    return object : Lazy<T> {
+
+        private var cached: T? = null
+
+        override val value: T
+            get() {
+                if (cached == null) {
+                    cached = T::class.java.getDeclaredMethod(
+                        "inflate", LayoutInflater::class.java,
+                        ViewGroup::class.java, Boolean::class.java
+                    ).invoke(null, LayoutInflater.from(this@viewBinding), parent, attachToParent) as T
+                }
+                return cached!!
+            }
+
+        override fun isInitialized() = cached != null
+    }
+}
+
+
+
+//-------------Collections----------------
+/**
+ * Moves the given **T** item to the specified index
+ */
+fun <T> MutableList<T>.move(item: T, newIndex: Int) {
+    val currentIndex = indexOf(item)
+    if (currentIndex < 0) return
+    removeAt(currentIndex)
+    add(newIndex, item)
+}
+
+/**
+ * Moves the given item at the `oldIndex` to the `newIndex`
+ */
+fun <T> MutableList<T>.moveAt(oldIndex: Int, newIndex: Int) {
+    val item = this[oldIndex]
+    removeAt(oldIndex)
+    if (oldIndex > newIndex)
+        add(newIndex, item)
+    else
+        add(newIndex - 1, item)
+}
+
+/**
+ * Check that index is in bound or not
+ */
+private fun <T> List<T>.isInBounds(index: Int): Boolean {
+    return index in 0..lastIndex
+}
+
+/**
+ * Swaps the two items
+ */
+fun <T> List<T>.swap(i: Int, j: Int): List<T> {
+    if (isInBounds(i) && isInBounds(j)) {
+        Collections.swap(this, i, j)
+    }
+    return this
+}
+
+/**
+ * Swaps the two items
+ */
+fun <T> MutableList<T>.swap(itemOne: T, itemTwo: T) = swap(indexOf(itemOne), indexOf(itemTwo))
+
+/**
+ * do something if Collection contains item
+ */
+fun <T> Collection<T>.doIfContained(item: T, func: T.() -> Unit): Boolean {
+    if (contains(item)) {
+        item.func()
+        return true
+    }
+    return false
+}
+
+/**
+ * add this value in given lists
+ */
+fun <T> T.addTo(vararg lists: MutableList<T>) {
+    lists.fold(Unit) { _, list ->
+        list.add(this)
+    }
+}
+
+/**
+ * remove this value from given lists
+ */
+fun <T> T.removeFrom(vararg lists: MutableList<T>) {
+    lists.fold(Unit) { _, list ->
+        list.remove(this)
+    }
+}
+
+/**
+ * remove this value from give values if given prediction is true
+ */
+fun <T> T.removeFrom(vararg lists: MutableList<T>, predicate: (T, T) -> Boolean) {
+    lists.fold(Unit) { _, list ->
+        list.removeAt(list.indexOfFirst { predicate(this, it) })
+    }
+}
+
+/**
+ * remove all this value from give lists
+ */
+fun <T> T.removeAllFrom(vararg lists: MutableList<T>) {
+    lists.fold(Unit) { _, list ->
+        list.removeAll { it == this }
+    }
+}
+
+/**
+ * remove all this value from given lists if prediction in true
+ */
+fun <T> T.removeAllFrom(vararg lists: MutableList<T>, predicate: (T, T) -> Boolean) {
+    lists.fold(Unit) { _, list ->
+        list.removeAll { predicate(this, it) }
+    }
+}
+
+/**
+ * return count of this value in given list
+ */
+fun <T> T.countIn(list: MutableList<T>) = list.count { it == this }
+
+/**
+ * Returns true if an element matching the given [predicate] was found.
+ */
+inline fun <T> Iterable<T>.contains(predicate: (T) -> Boolean): Boolean {
+    for (element in this) if (predicate(element)) return true
+    return false
+}
+
+/**
+ * Remove duplicate values
+ */
+inline fun <reified T> Collection<T>.removeDuplicates(): MutableList<T> {
+    return this.toSet().toMutableList()
+}
+
+
+//-------------Date/Time----------------
+fun Date.millisecondsSince(date: Date) = (time - date.time)
+fun Date.secondsSince(date: Date): Double = millisecondsSince(date) / 1000.0
+fun Date.minutesSince(date: Date): Double = secondsSince(date) / 60
+fun Date.hoursSince(date: Date): Double = minutesSince(date) / 60
+fun Date.daysSince(date: Date): Double = hoursSince(date) / 24
+fun Date.weeksSince(date: Date): Double = daysSince(date) / 7
+fun Date.monthsSince(date: Date): Double = weeksSince(date) / 4
+fun Date.yearsSince(date: Date): Double = monthsSince(date) / 12
+
+/**
+ * Gives [Calendar] object from Date
+ */
+inline val Date.calendar: Calendar
+    get() {
+        val calendar = Calendar.getInstance()
+        calendar.time = this
+        return calendar
+    }
+
+
+/**
+* Gets  Year directly from [Calendar] Object
+*/
+inline val Calendar.year: Int
+    get() = get(Calendar.YEAR)
+
+/**
+ * Gets value of DayOfMonth from [Calendar] Object
+ */
+inline val Calendar.dayOfMonth: Int
+    get() = get(Calendar.DAY_OF_MONTH)
+
+/**
+ * Gets value of Month from [Calendar] Object
+ */
+inline val Calendar.month: Int
+    get() = get(Calendar.MONTH)
+
+/**
+ * Gets value of Hour from [Calendar] Object
+ */
+inline val Calendar.hour: Int
+    get() = get(Calendar.HOUR)
+
+/**
+ * Gets value of HourOfDay from [Calendar] Object
+ */
+inline val Calendar.hourOfDay: Int
+    get() = get(Calendar.HOUR_OF_DAY)
+
+/**
+ * Gets value of Minute from [Calendar] Object
+ */
+inline val Calendar.minute: Int
+    get() = get(Calendar.MINUTE)
+
+/**
+ * Gets value of Second from [Calendar] Object
+ */
+inline val Calendar.second: Int
+    get() = get(Calendar.SECOND)
+
+/**
+ * Gets value of DayOfMonth from [Date] Object
+ */
+inline val Date.yearFromCalendar: Int
+    get() = calendar.year
+
+/**
+ * Gets value of DayOfMonth from [Date] Object
+ */
+inline val Date.dayOfMonth: Int
+    get() = calendar.dayOfMonth
+
+/**
+ * Gets value of Month from [Date] Object
+ */
+inline val Date.monthFromCalendar: Int
+    get() = calendar.month
+
+/**
+ * Gets value of Hour from [Date] Object
+ */
+inline val Date.hour: Int
+    get() = calendar.hour
+
+/**
+ * Gets value of HourOfDay from [Date] Object
+ */
+inline val Date.hourOfDay: Int
+    get() = calendar.hourOfDay
+
+/**
+ * Gets value of Minute from [Date] Object
+ */
+inline val Date.minute: Int
+    get() = calendar.minute
+
+
+/**
+ * Gets value of Second from [Date] Object
+ */
+inline val Date.second: Int
+    get() = calendar.second
+
+/**
+ * Converts current date to proper provided format
+ */
+fun Date.convertTo(format: String): String? {
+    var dateStr: String? = null
+    val df = SimpleDateFormat(format)
+    try {
+        dateStr = df.format(this)
+    } catch (ex: Exception) {
+        ex.printStackTrace()
+    }
+
+    return dateStr
+}
+
+fun Date.isFuture(): Boolean {
+    return !Date().before(this)
+}
+
+fun Date.isPast(): Boolean {
+    return Date().before(this)
+}
+
+fun Date.isToday(): Boolean {
+    return DateUtils.isToday(this.time)
+}
+
+fun Date.isYesterday(): Boolean {
+    return DateUtils.isToday(this.time + DateUtils.DAY_IN_MILLIS)
+}
+
+fun Date.isTomorrow(): Boolean {
+    return DateUtils.isToday(this.time - DateUtils.DAY_IN_MILLIS)
+}
+
+
+//-------------File----------------
+fun File.name() = this.nameWithExtension().substringBeforeLast(".")
+
+fun File.nameWithExtension() = this.absolutePath.substringAfterLast("/")
+
+fun File.extension() = this.absolutePath.substringAfterLast(".")
+
+
+//-------------Gson----------------
+fun Any.toJson(): String = Gson().toJson(this)
+
+inline fun <reified T : Any> String.toData(): T = Gson().fromJson(this, T::class.java)
